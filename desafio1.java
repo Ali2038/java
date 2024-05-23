25de26586cbf8eac8683446497863a688cfd5112
Ali <150735175+Ali2038@users.noreply.github.com>
2024-05-23 14:56:54 -0300

    desafio1.java

--- desafio1.java
+++ desafio1.java
@@ -3,8 +3,6 @@
 Lógica: O Squad tem uma necessidade em identificar se um cadastro é ativo ou desativo e de informar o nome completo do aluno. Desenvolver um algoritmo em Java que declare as variáveis necessárias, atribua valores de teste e visualize os valores compatíveis com esta lógica. Neste desafio você pode utilizar tipos de variáveis além dos primitivos, porém o aluno deve pesquisar.*/
 public class Main {
 
-public class Main {
-
 public static void main(String[] args) {
         String cadNomeAluno = "Denys Alves da Silva";
         boolean estadoAluno = true;
