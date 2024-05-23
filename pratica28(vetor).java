ce053342f1c83c99493dbd3fc181f52f151ab19d
Ali <150735175+Ali2038@users.noreply.github.com>
2024-05-23 16:48:11 -0300

    Add files via upload
    Vetor – Nomes na ordem inversa de entrada

---
+++ pratica28(vetor).java
@@ -0,0 +1,46 @@
+/*Vetor – Nomes na ordem inversa de entrada*/
+
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args) {
+
+    //criacao do vetor
+
+    String[] nomes = new String[5];
+
+
+    // Criação do objeto de entrada de dados
+
+    Scanner entradaVetor = new Scanner([http://System.in](http://System.in "smartCard-inline") );
+
+
+    //loop
+
+    for (int indiceVetor = 0; indiceVetor < nomes.length; indiceVetor++)
+
+    {
+
+        System.out.println(" Entre com um nome : ");
+
+        nomes[indiceVetor] = entradaVetor.nextLine();
+
+    }
+
+
+    for ( int indiceVetor = nomes.length - 1; indiceVetor >= 0; indiceVetor--)
+
+    {
+
+        System.out.println("o nome no indice " + indiceVetor +": "+ nomes[indiceVetor] );
+
+    }
+
+
+
+
+}
+
+
+}
