e5b86c09e4cb663c2f7129ab4272919db8d90c71
Ali <150735175+Ali2038@users.noreply.github.com>
2024-05-23 14:48:03 -0300

    Add files via upload

---
+++ pratica03.java
@@ -0,0 +1,14 @@
+/*Desenvolver um algoritmo em Java para declarar duas variáveis. A necessidade do usuário é em dividir um valor qualquer por 3, sendo assim o algoritmo tem que ter uma variável para o número a ser divido e outra variável para o número do resultado da divisão. Atribua e visualize os valores compatíveis com esta lógica.*/
+
+public class Main
+{
+public static void main(String[] args)
+{
+// Declaração e atribuição
+double nroDividendo = 34786.34;
+float nroQuociente = 1234.1234f;
+// Visualização dos valores atribuidos
+System.out.printf("Dividendo: %,.2f\n", nroDividendo);
+System.out.printf("Quociente: %,.5f\n", nroQuociente);
+}
+}
